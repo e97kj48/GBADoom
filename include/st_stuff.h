67accaa4ef9ae00e9f200905db7d2d412fe75ee5
e97kj48 <121514598+e97kj48@users.noreply.github.com>
2023-01-03 23:06:31 -0600

    Turned ST_FACES into an aiming reticle

--- include/st_stuff.h
+++ include/st_stuff.h
@@ -99,7 +99,7 @@
 
 // proff 08/18/98: Changed for high-res
 #define ST_FACESX               (ST_X+104)
-#define ST_FACESY               (ST_Y)
+#define ST_FACESY               (ST_Y-48)
 
 #define ST_EVILGRINCOUNT        (2*TICRATE)
 //Fix Status bar face timing ~Kippykip
